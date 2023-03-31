b038c1ebf4b2c1e2fb7c610cc6be549bd99c0ff6
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-03-31 14:40:15 +0200

    install next.js and folder structure

---
+++ src/app/head.tsx
@@ -0,0 +1,15 @@
+import Head from "next/head";
+
+function IndexPage() {
+  return (
+    <>
+      <Head>
+        <title>My page title</title>
+        <meta name="description" content="This is the clone of IMDB website" />
+        <link rel="icon" href="./favicon.ico" />
+      </Head>
+    </>
+  );
+}
+
+export default IndexPage;
