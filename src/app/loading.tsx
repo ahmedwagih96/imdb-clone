2f08a782713f8aaa21732f2c61c4ad5ae69f6f73
Ahmed Wagih <ahmd.wagih96@gmail.com>
2023-04-03 01:02:38 +0200

    added loading component

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import FadeLoader  from "react-spinners/FadeLoader";
 type Props = {}
 
 function Loading({}: Props) {
   return (
     <div className='flex justify-center'>
-        <img className ='h-96'src="spinner.svg" alt="loading..." />
+        {/* <img className ='h-96'src="spinner.svg" alt="loading..." /> */}
+        <FadeLoader color="#F59E0B" />
     </div>
   )
 }
